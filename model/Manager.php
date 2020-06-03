90425723b4e8388274505d906b5d53f2d0a5c0fb
Michel Merra <Mitch@MacBook-Pro-de-Michel.local>
2020-06-03 11:40:46 +0200

    Test sur divers vues et back

--- model/Manager.php
+++ model/Manager.php
@@ -1,3 +1,16 @@
 <?php
 
-?>
+class Manager
+{
+    protected function dbConnect()
+    {
+        $db = new PDO('mysql:host=localhost;dbname=forteroche;charset=utf8', 'root', '');
+        $password=password_hash('ani',PASSWORD_DEFAULT);
+        $query=$db->prepare('INSERT INTO users (id, login, pass) VALUES(33, ?, ?)');
+        $query->execute(['ani',$password]);
+        return $db;
+   
+    }
+   
+
+}
