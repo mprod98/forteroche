de67178007b950aec5a1070d3e8773d468291d1f
Michel Merra <Mitch@MacBook-Pro-de-Michel.local>
2020-07-07 12:36:57 +0200

    extraction des données configuration

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # forteroche
+Une fois le depot cloné renommez le fichier config.php.exemple en config.php et complétez le.
--- model/Manager.php
+++ model/Manager.php
@@ -4,11 +4,8 @@ class Manager
 {
     protected function dbConnect()
     {
-        $db = new PDO('mysql:host=localhost;dbname=forteroche;charset=utf8', 'root', '');
-        //$password=password_hash('ani',PASSWORD_DEFAULT);
-        //$query=$db->prepare('INSERT INTO users (id, login, pass) VALUES(33, ?, ?)');
-        //$query->execute(['ani',$password]);
-
+        require ('config.php');
+        $db = new PDO('mysql:host='.$CONFIG['host'].';dbname='.$CONFIG['dbname'].';charset=utf8',$CONFIG['user'], $CONFIG['password']);
         return $db;
         
    
